0d16a4cbb67817903add348831b46268e5012f54
KamauIan <kamauiyan@gmail.com>
2018-10-02 12:14:37 +0300

    Added Drawer

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import NewsFeed from './NewsFeed';
 import Settings from './Settings';
-// import Head from './Header';
-// import { Content, Container } from 'native-base';
+
 import { SafeAreaView, ScrollView, View, Image, Dimensions } from 'react-native';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation';
-
 
 export default class Main extends Component {
   render() {
     return (
-      <Container>
-        <Head />
-      <Content>
       <AppDrawerNavigator />
-      </Content>
-      </Container>
     );
   }
 }
+// const {width} = Dimensions.get('window');
+const CustomDrawerComponent = (props) => (
+  <SafeAreaView style={{ flex: 1 }}>
+  <View style={{ height:150, backgroundColor:'#000', alignItems:'center', justifyContent:'center' }}>
+    <Image source={{uri: 'https://i.stack.imgur.com/ILTQq.png'}} style={{height:120, width:120}}/>
+  </View>
+    <ScrollView>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+)
+const AppDrawerNavigator = createDrawerNavigator({
+  NewsFeed: NewsFeed,
+  Settings: Settings
+}, {
+    contentComponent: CustomDrawerComponent,
+    drawerWidth: 250
+  }
+)
