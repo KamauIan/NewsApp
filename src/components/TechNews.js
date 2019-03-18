e214a006d195ab36089178479f9ae2d64620e35c
waweru kamau <wawerumail@gmail.com>
2019-03-18 10:14:30 +0300

    packages update, ui tweaks

--- src/components/TechNews.js
+++ src/components/TechNews.js
@@ -1,32 +1,39 @@
-import React, { Component } from 'react';
-import { FlatList, View } from 'react-native';
-import NewsCard from './NewsCard';
-import Head from './Header';
-import { Icon } from 'native-base';
-import NewsDetail from './NewsDetail';
-import { createStackNavigator } from 'react-navigation';
+import React, { Component } from "react";
+import { FlatList, View } from "react-native";
+import NewsCard from "./NewsCard";
+import Head from "./Header";
+import { Icon } from "native-base";
+import NewsDetail from "./NewsDetail";
+import { createStackNavigator } from "react-navigation";
 
-const url = "https://newsapi.org/v2/top-headlines?category=technology&apiKey=6ca34c820cce443f9ae46a62aef41555";
+const url =
+  "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=6ca34c820cce443f9ae46a62aef41555";
 
 class NewsFeeds extends Component {
   constructor(props) {
     super(props);
-    this.state = { articles: [], refreshing: true };                //initialize states
-    this.fetchNews = this.fetchNews.bind(this)
+    this.state = { articles: [], refreshing: true }; //initialize states
+    this.fetchNews = this.fetchNews.bind(this);
   }
-  componentDidMount() {                                                // call the fetch function immediately the component mounts
+  componentDidMount() {
+    // call the fetch function immediately the component mounts
     this.fetchNews();
   }
- 
-  async fetchNews() {                                                   // Fetch news from news api url asynchronously
-    await fetch(url).then(response => response.json())
+
+  async fetchNews() {
+    // Fetch news from news api url asynchronously
+    await fetch(url)
+      .then(response => response.json())
       .then(articles => this.setState({ articles, refreshing: false })) //set the state of articles[]
-      .catch((err) => {
-        this.setState({ refreshing: false })
-        console.log("The articles state is not set. Error: ", err)
-      })
-    console.log("The articles found were: ", this.state.articles)
-    console.log("\nAnd The first articles is: ", this.state.articles.articles[0].title)
+      .catch(err => {
+        this.setState({ refreshing: false });
+        console.log("The articles state is not set. Error: ", err);
+      });
+    console.log("The articles found were: ", this.state.articles);
+    console.log(
+      "\nAnd The first articles is: ",
+      this.state.articles.articles[0].title
+    );
   }
 
   handleRefresh() {
@@ -38,27 +45,28 @@ class NewsFeeds extends Component {
     );
   }
   static navigationOptions = {
-    header: <Head
-      headerTitle="Tech News"
-      IconName="menu"
-    />
-  }
+    header: <Head headerTitle="Tech News" IconName="menu" />
+  };
   render() {
     return (
       <FlatList
         data={this.state.articles.articles}
-        renderItem={({ item }) =>
+        renderItem={({ item }) => (
           <NewsCard
             newscard={item}
-            OpenDetails={() => this.props.navigation.navigate('NewsDetail',  //OpenDetils is a prop created in NewsCard
-              {
-                url: item.url,
-                title: item.title,
-                urlToImage: item.urlToImage,
-                content: item.content
-              }
-            )}
-          />}
+            OpenDetails={() =>
+              this.props.navigation.navigate(
+                "NewsDetail", //OpenDetils is a prop created in NewsCard
+                {
+                  url: item.url,
+                  title: item.title,
+                  urlToImage: item.urlToImage,
+                  content: item.content
+                }
+              )
+            }
+          />
+        )}
         keyExtractor={item => item.url}
         refreshing={this.state.refreshing}
         onRefresh={this.handleRefresh.bind(this)}
@@ -68,9 +76,8 @@ class NewsFeeds extends Component {
 }
 
 export default class TechNews extends Component {
-  
   render() {
-    return (<RootStack />)
+    return <RootStack />;
   }
 }
 
@@ -81,6 +88,6 @@ const RootStack = createStackNavigator(
     TechNews: TechNews
   },
   {
-    initialRouteName: 'NewsFeeds',
+    initialRouteName: "NewsFeeds"
   }
 );
